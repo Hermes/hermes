8338843baa49547ce280754d152844b22bd4fbdb
Matt Olan <olanmatt@gmail.com>
2013-05-18 15:36:16 -0400

    Basic compression working

--- client/compression.go
+++ client/compression.go
@@ -3,33 +3,50 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"code.google.com/p/lzma"
+	"io"
+	"os"
 )
 
-func compress() {
+func compress(file string) {
+	in, _ := os.Open(file)
+	defer in.Close()
+	out, _ := os.Create(file + "lz")
+	defer out.Close()
+	w := lzma.NewWriter(out)
 
+	// make a buffer to keep chunks that are read
+    buf := make([]byte, 1024)
+    for {
+        // read a chunk
+        n, err := in.Read(buf)
+        if err != nil && err != io.EOF { panic(err) }
+        if n == 0 { break }
+
+        w.Write(buf[:n])
+        /* write a chunk
+        if _, err := out.Write(buf[:n]); err != nil {
+            panic(err)
+        }*/
+    }
+	w.Close()
 }
 
-func decompress() {
-
+func decompress(file string) {
+	in, _ := os.Open(file)
+	defer in.Close()
+	r := lzma.NewReader(in)
+	io.Copy(os.Stdout, r)
+	r.Close()
 }
 
 func main() {
 	
+	compress("test.mp4")
+	//decompress("test.doclz")
 
-	// Write compressed data to a buffer:
-	var b bytes.Buffer
-	w := lzma.NewWriter(&b)
-	w.Write([]byte("hello, world\n"))
-	w.Close()
-
-	// and read it back:
-	r := lzma.NewReader(&b)
-	io.Copy(os.Stdout, r)
-	r.Close()
-
-
+	/*
 
 	// If the data is bigger than you'd like to hold into memory, use pipes. Write compressed data to an io.PipeWriter:
 	pr, pw := io.Pipe()
@@ -48,5 +65,7 @@ func main() {
 	defer r.Close()
 	// the os.Stdout would be an io.Writer used to write uncompressed data to
 	io.Copy(os.Stdout, r)
+
+	*/
 
 }
