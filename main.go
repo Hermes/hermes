a05866a27faacd4e4f5a68247feb38d9feb80257
Matt Olan <olanmatt@gmail.com>
2013-05-19 01:48:45 -0400

    Improved flag validation

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ package main
 import (
 	//"hermes/client"
 	//"hermes/server"
-	//"os"
+	"os"
 	//"io"
 	"flag"
 	"fmt"
 )
 
-func generate() {
+func generate(file string, pass string) {
 	fmt.Println("generate")
 }
 
@@ -23,20 +23,12 @@ func update() {
 	fmt.Println("update")
 }
 
-func pull() {
+func pull(file string) {
 	fmt.Println("pull")
 }
 
-func push() {
+func push(file string) {
 	fmt.Println("push")
-}
-
-func ls() {
-	fmt.Println("ls")
-}
-
-func cd() {
-	fmt.Println("cd")
 }
 
 func lock() {
@@ -44,7 +36,12 @@ func lock() {
 }
 
 func flagValidate(flags []string) bool {
-	allFlags := []string{"generate", "update", "pull", "push", "ls", "cd", "lock"}
+	allFlags := []string{"generate", "update", "pull", "push", "lock"}
+
+	// Validating number of flags
+	if len(flags) == 0 || len(flags) >=  3 {
+		return false
+	}
 
 	// Validating one flag is passed
 	count := 0
@@ -58,36 +55,48 @@ func flagValidate(flags []string) bool {
         	}
 		}
     }
+
+    // Validating for specific cases
+    switch flags[0] { 
+		case "update":
+			if len(flags) != 1 {
+				return false
+			}
+		case "generate":
+			if len(flags) < 2 || len(flags) > 3 {
+				return false
+			} else if len(flags) == 2 {
+				flags = append(flags, "")
+			}
+		case "pull":
+			if len(flags) != 2 {
+				return false
+			}
+		case "push":
+			if len(flags) != 2 {
+				return false
+			} else if _, err := os.Stat(flags[1]); os.IsNotExist(err) {
+				return false
+			}
+		case "lock":
+			if len(flags) != 1 {
+				return false
+			}
+		default: return false
+	}
 	return true
-}
-
-func flagExists(flags []string, flag string) bool {
-    for _, key := range flags {
-        if (flag == key) {
-            return true
-        }
-    }
-    return false
-}
-
-func flagParse(flags []string) string {
-    return flags[0]
 }
 
 func main() {
 
-	// Flag parsing
 	flag.Parse()
 	flags := flag.Args()
 	if flagValidate(flags) {
-		flag := flagParse(flags)
-		switch flag { 
+		switch flags[0] { 
 			case "update": update()
-			case "generate": generate()
-			case "pull": pull()
-			case "push": push()
-			case "ls": ls()
-			case "cd": cd()
+			case "generate": generate(flags[1], flags[2])
+			case "pull": pull(flags[1])
+			case "push": push(flags[1])
 			case "lock": lock()
 			default: fmt.Println("Error: Invalid Flags")
 		}
