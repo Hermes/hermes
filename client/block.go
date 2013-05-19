f2b8bc44ff70db16c4b86476e8945920cd84f5bd
William-Mak <william.mak@hotmail.ca>
2013-05-18 23:12:34 -0400

    Renaming to block.go, Removing functinons that were completely pointless.

---
+++ client/block.go
@@ -0,0 +1,94 @@
+package client
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+	"path"
+	"strings"
+)
+
+//Split: Splits a 'file' into blocks of size 'block_size' and outputs them into
+//The directory given by 'filedir'
+func Split(file io.Reader, block_size int, filedir string) []string {
+	final_files := make([]string, 0)
+	buf := make([]byte, block_size)
+	//Open the file
+	file = bufio.NewReader(file)
+	for {
+		// read a chunk
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		if n == 0 {
+			break
+		}
+
+		//Hash the file with SHA256 to determine a unique filename
+		h := sha256.New()
+		s := string(buf[:n])
+		io.WriteString(h, s)
+		//Creating the filename by appending filedir with the SHA256 Hash
+		filename := path.Join(filedir, hex.EncodeToString(h.Sum(nil)))
+
+		//Write the file and add it's relative path to the list
+		fo, err := os.Create(filename)
+		// fo, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+		// close fo on exit and check for its returned error
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		// make a write buffer
+		w := bufio.NewWriter(fo)
+		if _, err := w.Write(buf[:n]); err != nil {
+			panic(err)
+		}
+		if err = w.Flush(); err != nil {
+			panic(err)
+		}
+		final_files = append(final_files, filename)
+	}
+	return final_files
+}
+
+//Join: Takes a list of 'files' and joins them back together according to it's order
+func Join(files []string, block_size int) io.Reader {
+	result := make([]byte, 0)
+	for _, file := range files {
+		input_file, err := os.Open(file)
+		if err != nil {
+			panic(err)
+		}
+		// close fi on exit and check for its returned error
+		defer func() {
+			if err := input_file.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		//read all the bytes from the file
+		r := bufio.NewReader(input_file)
+		buf := make([]byte, block_size)
+		n, err := r.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+
+		//combine the bytes together
+		for _, bite := range buf[:n] {
+			result = append(result, bite)
+		}
+	}
+
+	//create an io.Reader and return it
+	sresult := string(result)
+	return strings.NewReader(sresult)
+}
